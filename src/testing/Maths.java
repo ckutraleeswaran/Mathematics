87d6a9e6977d28a9ee71f85b0803846531d33724
ajithsathyan7 <ajithsathyan77@gmail.com>
2025-11-13 12:42:54 +0530

    push modified

--- src/testing/Maths.java
+++ src/testing/Maths.java
@@ -2,11 +2,22 @@ package testing;
 
 public class Maths{
 	
-	
+	public void sub() {
+		
+		int a = 10;
+		int b = 20;
+		
+		int c = b-a;
+		System.out.println(c);
+		
+		
+		
+		
+	}
 	public static void main(String[] args) {
 		
 		Maths obj = new Maths();
-		
+		obj.sub();
 	
 	}
 	
