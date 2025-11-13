2ccd74ce7be70a1afd387bae4924155457513425
kannanselvan <thiru.kannanselvan@gmail.com>
2025-11-13 12:42:49 +0530

    Addition Complited

--- src/testing/Maths.java
+++ src/testing/Maths.java
@@ -1,12 +1,17 @@
 package testing;
 
 public class Maths{
-	
+	public void addition () {
+		int a = 100;
+		int b = 200;
+		int c = a+b;
+		System.out.println(c);
+	}
 	
 	public static void main(String[] args) {
 		
 		Maths obj = new Maths();
-		
+		obj.addition();
 	
 	}
 	
